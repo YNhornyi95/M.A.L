c0460f69411fce9ca44f1a7d90444ca9f41bbdf4
Женя Нагорный <yevhen_nahornyi@icloud.com>
2020-10-06 10:07:16 +0300

    + Lab M1 1_2

--- LabsYN/Labs.cs
+++ LabsYN/Labs.cs
@@ -6,11 +6,7 @@ namespace LabsYN
     {
         static void Main(string[] args)
         {
-            string a = "Zhora ";
-            Console.WriteLine("Вассяяя!");
-            Console.WriteLine("UUUggiin!");
-            string b = Console.ReadLine();
-            Console.WriteLine(a + b);
+            
         }
     }
 }
